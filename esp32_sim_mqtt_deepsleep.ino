eac195b6a69dbbecc7a98902d6d1eb947760f7fc
Markusenz <info@emtex.de>
2025-12-17 01:46:32 +0100

    Use WalterModem in ESP32 example

--- esp32_sim_mqtt_deepsleep.ino
+++ esp32_sim_mqtt_deepsleep.ino
@@ -1,9 +1,9 @@
-// Beispielcode für ESP32 mit SIM-Modem (z.B. SIM800/7000) und MQTT
-// Voraussetzungen: TinyGSM, PubSubClient und passende Board-/Modem-Treiber installiert
+// Beispielcode für ESP32 Walter Board mit WalterModem-Library und MQTT
+// Voraussetzungen: WalterModem (liefert GSM/LTE-Treiber + Client), PubSubClient
 // Verbindet sich über Mobilfunk (APN), sendet eine MQTT-Nachricht und geht anschließend für 10 Minuten in den Deep Sleep.
 
 #include <Arduino.h>
-#include <TinyGsmClient.h>
+#include <WalterModem.h>
 #include <PubSubClient.h>
 
 // --- Benutzerkonfiguration ---
@@ -15,21 +15,20 @@ static const uint16_t MQTT_PORT   = 1883;
 static const char MQTT_TOPIC[]    = "esp32/walter/status";
 static const char MQTT_PAYLOAD[]  = "Hallo vom ESP32 über SIM!";
 
-// GPIO für das Modem (an Board/Modul anpassen)
+// GPIO für das Walter-Modem (an Board/Modul anpassen)
 static const int MODEM_PWR_PIN    = 23;  // Power-Key oder Enable-Pin des Modems
 static const int MODEM_RST_PIN    = 5;   // Reset-Pin des Modems
 static const int MODEM_TX_PIN     = 27;  // TX vom ESP32 zum Modem
 static const int MODEM_RX_PIN     = 26;  // RX zum ESP32 vom Modem
 
-#define TINY_GSM_MODEM_SIM800
-// #define TINY_GSM_MODEM_SIM7000   // Alternativ aktivieren, falls SIM7000 genutzt wird
-
-TinyGsm modem(Serial1);
-TinyGsmClient gsmClient(modem);
-PubSubClient mqttClient(gsmClient);
-
 // 10 Minuten Deep Sleep (in Mikrosekunden)
 static const uint64_t SLEEP_TIME_US = 10ULL * 60ULL * 1000000ULL;
+
+// WalterModem übernimmt das Ansteuern des Modems und stellt einen Client für MQTT bereit
+HardwareSerial SerialAT(1);
+WalterModem walterModem(&SerialAT, MODEM_PWR_PIN, MODEM_RST_PIN);
+Client &netClient = walterModem.getClient();
+PubSubClient mqttClient(netClient);
 
 void setupModemGPIO()
 {
@@ -41,24 +40,26 @@ void setupModemGPIO()
 
 bool connectModem()
 {
-    Serial.println("Modem wird gestartet...");
-    Serial1.begin(115200, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
+    Serial.println("Modem wird gestartet (WalterModem)...");
+    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
 
-    if (!modem.restart())
+    if (!walterModem.begin())
     {
-        Serial.println("Modem-Start fehlgeschlagen");
+        Serial.println("WalterModem konnte nicht initialisiert werden");
         return false;
     }
 
+    walterModem.configureAPN(APN, GPRS_USER, GPRS_PASS);
+
     Serial.println("Netzwerkanmeldung...");
-    if (!modem.gprsConnect(APN, GPRS_USER, GPRS_PASS))
+    if (!walterModem.connectNetwork())
     {
-        Serial.println("GPRS-Verbindung fehlgeschlagen");
+        Serial.println("GPRS/LTE-Verbindung fehlgeschlagen");
         return false;
     }
 
     Serial.print("Verbunden, IP: ");
-    Serial.println(modem.localIP());
+    Serial.println(walterModem.localIP());
     return true;
 }
 
@@ -84,7 +85,7 @@ bool publishOnce()
 void goToDeepSleep()
 {
     Serial.println("Gehe jetzt in Deep Sleep...");
-    modem.poweroff();
+    walterModem.powerOff();
     esp_sleep_enable_timer_wakeup(SLEEP_TIME_US);
     esp_deep_sleep_start();
 }
@@ -112,7 +113,7 @@ void setup()
     }
 
     // Trennung der Datenverbindung und Sleep
-    modem.gprsDisconnect();
+    walterModem.disconnectNetwork();
     goToDeepSleep();
 }
 
@@ -120,4 +121,3 @@ void loop()
 {
     // Wird aufgrund Deep Sleep nicht erreicht
 }
-
